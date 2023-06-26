5dfff0b90b60dbe737f05e26df7a09bf8dda8480
gabobriceno83 <56813281+gabobriceno83@users.noreply.github.com>
2023-06-25 20:25:16 -0500

    Agrega Ejercicio 1 de la clase 5

---
+++ Ejercicio1.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main (String [] args) {
+
+       // Calculo del IMC:  Peso en Kg/ Altura en mtr al cuadrado
+
+        System.out.println("Ingrese su peso en Kg");
+        Scanner cargaDeDatos = new Scanner(System.in);
+        double peso = cargaDeDatos.nextDouble();
+        System.out.println("Ingrese su altura en mts");
+        double altura = cargaDeDatos.nextDouble();
+        double imc = peso /(altura * altura);
+        System.out.println("Tu indice de Masa Corporal es: "+ imc);
+
+
+
+        //Mayor a 30 ---> Obesidad
+        if (imc >= 30){
+            System.out.println("Presentas Obesidad");
+        }
+
+        //Entre a 25 y  30 ---> Sobrepeso
+        else if (imc >=25 && imc < 30) {
+            System.out.println("Presentas Sobrepeso");
+        }
+        //Entre 18.5 y 25 ----> Normal
+        else if (imc >=18.5 && imc < 25) {
+            System.out.println("Presenta un peso normal ");
+        }
+        //Menor a 18.6 ------> Debajo de lo normal
+        else if (imc < 18.6 ) {
+            System.out.println("Presenta una falta de peso");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       /*
+        System.out.println("¿Que edad tienes?");
+        Scanner cargaDeDatos = new Scanner(System.in);
+        Integer edad = cargaDeDatos.nextInt();
+
+        if (edad >=18) {
+             System.out.println("Es mayor de edad");
+
+        } else {
+            System.out.println("Es menor de edad");
+
+        }*/
+    }
+}
